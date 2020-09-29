cdbe5707d95c9f4f78a91b0d5bf2cb9353cc7c6d
samuel <samuelolayemi221@yahoo.com>
2020-09-29 19:56:09 +0200

    https://github.com/sammy007-debug/checkers.git

---
+++ CHECKER/checkers/constants.py
@@ -0,0 +1,20 @@
+import pygame
+
+
+WIDTH = 600 
+HEIGHT = 600
+
+ROWS, COLS = 8, 8
+
+SQUARE_SIZE = WIDTH // COLS
+
+#rgb
+
+RED = (255, 0, 0)
+WHITE = (255, 255, 255)
+BLACK = (0, 0, 0)
+BLUE = (0, 0, 255)
+GREY = (128, 128, 128)
+
+
+CROWN = pygame.transform.scale(pygame.image.load("C:\CheckersGame/CHECKER/checkers/assets/assets/crown.png"), (44, 25))
